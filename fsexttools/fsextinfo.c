7c0d423e39812d4a258fca359318199b3b289f82
Joachim Metz <joachim.metz@gmail.com>
2018-01-29 07:27:56 +0100

    Applied updates

--- fsexttools/fsextinfo.c
+++ fsexttools/fsextinfo.c
@@ -1,7 +1,7 @@
 /*
  * Shows information obtained from an Extended File System (ext) volume
  *
- * Copyright (C) 2010-2017, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2010-2018, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
